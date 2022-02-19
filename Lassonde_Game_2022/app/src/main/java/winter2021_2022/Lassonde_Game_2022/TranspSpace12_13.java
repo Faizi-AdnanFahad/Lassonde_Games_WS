97b52683700d468ef098ae3e06b9f26bd1d12216
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-02-19 00:57:08 -0500

    Trans ->3cat 12-13

--- Lassonde_Game_2022/app/src/main/java/winter2021_2022/Lassonde_Game_2022/TranspSpace12_13.java
+++ Lassonde_Game_2022/app/src/main/java/winter2021_2022/Lassonde_Game_2022/TranspSpace12_13.java
@@ -2,13 +2,27 @@ package winter2021_2022.Lassonde_Game_2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TranspSpace12_13 extends AppCompatActivity {
+    private Button flyingCars_12_13;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transp_space1213);
+
+        // changing from Age Page 8 - 9 -> Transportation and Space tourism
+        this.flyingCars_12_13 = findViewById(R.id.buttonTransp_12_13);
+        this.flyingCars_12_13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TranspSpace12_13.this, Trans3Cat_12_13.class);
+                startActivity(intent);
+            }
+        });
     }
 }
