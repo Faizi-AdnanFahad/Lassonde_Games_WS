507c46dc08e0d3a01ffe945eb3972b9030f278c7
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-02-19 00:05:20 -0500

    Transportation and Space Tourism complete

--- Lassonde_Game_2022/app/src/main/java/winter2021_2022/Lassonde_Game_2022/AgePage.java
+++ Lassonde_Game_2022/app/src/main/java/winter2021_2022/Lassonde_Game_2022/AgePage.java
@@ -9,18 +9,41 @@ import android.widget.Button;
 
 public class AgePage extends AppCompatActivity {
     private Button age_8_9;
+    private Button age_10_11;
+    private Button age_12_13;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age_page);
 
-        // changing from IntroPage -> AgePage
+        // changing from Age Page 8 - 9 -> Transportation and Space tourism
         this.age_8_9 = findViewById(R.id.button8_9);
         this.age_8_9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(AgePage.this, TranspSpace8_9.class);
+                startActivity(intent);
+            }
+        });
+
+        // changing from Age Page 10 - 11 -> Transportation and Space tourism
+        this.age_10_11 = findViewById(R.id.button10_11);
+        this.age_10_11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AgePage.this, TranspSpace10_11.class);
+                startActivity(intent);
+            }
+        });
+
+        // changing from Age Page 10 - 11 -> Transportation and Space tourism
+        this.age_12_13 = findViewById(R.id.button12_13);
+        this.age_12_13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AgePage.this, TranspSpace12_13.class);
                 startActivity(intent);
             }
         });
---
+++ Lassonde_Game_2022/app/src/main/java/winter2021_2022/Lassonde_Game_2022/TranspSpace10_11.java
@@ -0,0 +1,13 @@
+package winter2021_2022.Lassonde_Game_2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TranspSpace10_11 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transp_space1011);
+    }
+}
