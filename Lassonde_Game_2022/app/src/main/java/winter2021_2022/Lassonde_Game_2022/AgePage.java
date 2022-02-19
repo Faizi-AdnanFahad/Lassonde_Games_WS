1282751c25eb377ef3e0aa751591c01c757cfccc
Adnan Fahad Faizi <adnan500@my.yorku.ca>
2022-02-18 23:54:03 -0500

    Navigated Successfully from age8_9 -> TransSpace

--- Lassonde_Game_2022/app/src/main/java/winter2021_2022/Lassonde_Game_2022/AgePage.java
+++ Lassonde_Game_2022/app/src/main/java/winter2021_2022/Lassonde_Game_2022/AgePage.java
@@ -7,23 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainPage extends AppCompatActivity {
-    private Button followMe;
+public class AgePage extends AppCompatActivity {
+    private Button age_8_9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
+        setContentView(R.layout.activity_age_page);
 
         // changing from IntroPage -> AgePage
-        this.followMe = findViewById(R.id.buttonFollowMe);
-        this.followMe.setOnClickListener(new View.OnClickListener() {
+        this.age_8_9 = findViewById(R.id.button8_9);
+        this.age_8_9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainPage.this, AgePage.class);
+                Intent intent = new Intent(AgePage.this, TranspSpace8_9.class);
                 startActivity(intent);
             }
         });
     }
+
 
 }
--- Lassonde_Game_2022/app/src/main/java/winter2021_2022/Lassonde_Game_2022/TranspSpace8_9.java
+++ Lassonde_Game_2022/app/src/main/java/winter2021_2022/Lassonde_Game_2022/TranspSpace8_9.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginPage extends AppCompatActivity {
+public class TranspSpace8_9 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        setContentView(R.layout.activity_transp_space89);
     }
+
+
 }
